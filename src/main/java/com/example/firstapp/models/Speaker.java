bc5311d8ec84f2365370f5eca76d2e3778a0e962
Parth Patel <parth_patel@comcast.com>
2020-03-14 20:36:32 -0400

    add speaker photo to handle with large binary object

--- src/main/java/com/example/firstapp/models/Speaker.java
+++ src/main/java/com/example/firstapp/models/Speaker.java
@@ -1,5 +1,7 @@
 package com.example.firstapp.models;
 
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -16,10 +18,22 @@ public class Speaker {
     private String company;
     private String speaker_bio;
 
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    private byte[] speaker_photo;
+
     @ManyToMany(mappedBy = "speakers")
     private List<Session> sessions;
 
     public Speaker() {
+    }
+
+    public byte[] getSpeaker_photo() {
+        return speaker_photo;
+    }
+
+    public void setSpeaker_photo(byte[] speaker_photo) {
+        this.speaker_photo = speaker_photo;
     }
 
     public List<Session> getSessions() {
